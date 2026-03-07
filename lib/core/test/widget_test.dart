37a7e603cb87fc61c886f03b6d00463b7bdbbaca
Christian Ermias <christianermias@gmail.com>
2026-03-07 15:29:33 -0800

    remove unnecessary test file

--- lib/core/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:customer_app/main.dart';
-
-void main() {
-  testWidgets('App launches without crashing', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-    expect(find.byType(MyApp), findsOneWidget);
-  });
-}
